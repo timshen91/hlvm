236c312f056f3c5cc0e8ecc82b625ae8b598e3ed
tim <timshen91@gmail.com>
2014-02-03 15:50:44 -0500

    trivial

--- core.cpp
+++ core.cpp
@@ -1,44 +1,44 @@
 // ---------- file scope ----------
-NodePtr handle_file(const List &list) {
+NodePtr handle_file(const List& list) {
   cerr << "handle_file\n";
   return nullptr;
 }
 
 // ---------- comment ----------
-NodePtr handle_comment(const List &list) { return nullptr; }
+NodePtr handle_comment(const List& list) { return nullptr; }
 
 // ---------- function ----------
-NodePtr handle_function(const List &list) {
+NodePtr handle_function(const List& list) {
   cerr << "handle_function\n";
   return nullptr;
 }
 
-NodePtr handle_if(const List &list) {
+NodePtr handle_if(const List& list) {
   cerr << "handle_if\n";
   return nullptr;
 }
 
-NodePtr handle_lt(const List &list) {
+NodePtr handle_lt(const List& list) {
   cerr << "handle_lt\n";
   return nullptr;
 }
 
-NodePtr handle_return(const List &list) {
+NodePtr handle_return(const List& list) {
   cerr << "handle_return\n";
   return nullptr;
 }
 
-NodePtr handle_plus(const List &list) {
+NodePtr handle_plus(const List& list) {
   cerr << "handle_plus\n";
   return nullptr;
 }
 
-NodePtr handle_call(const List &list) {
+NodePtr handle_call(const List& list) {
   cerr << "handle_call\n";
   return nullptr;
 }
 
-NodePtr handle_minus(const List &list) {
+NodePtr handle_minus(const List& list) {
   cerr << "handle_minus\n";
   return nullptr;
 }
--- main.cpp
+++ main.cpp
@@ -93,7 +93,7 @@ class List {
 class Node {
  public:
   virtual void codegen() const = 0;
-  virtual ~Node() {}
+  virtual ~Node() = 0;
 };
 
 // FIXME Will be reimplemented in the future.
