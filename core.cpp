04ee3cb6559208f8c25ac60562c7c17ca4e9bb43
tim <timshen91@gmail.com>
2014-02-04 17:41:50 -0500

    Environment revised

--- core.cpp
+++ core.cpp
@@ -34,14 +34,36 @@ class Environment {
 
   Environment(Environment* parent) : parent(parent) {}
 
-  Type* lookup_type(String name) {
-    if (this == nullptr) {
-      return nullptr;
-    }
+  template <typename... Args>
+  Variable* emplace_var(Args&&... args) {
+    return emplace_impl(vars, args...);
+  }
+
+  template <typename... Args>
+  FunctionNode* emplace_func(Args&&... args) {
+    return emplace_impl(functions, args...);
+  }
+
+  template <typename... Args>
+  Type* emplace_type(Args&&... args) {
+    return emplace_impl(types, args...);
+  }
+
+  Type* must_lookup_type(String name) {
+    ensure(this, "No such type");
     if (types.count(name) == 0) {
-      return parent->lookup_type(name);
+      return parent->must_lookup_type(name);
     }
     return &types.find(name)->second;
+  }
+
+ private:
+  template <typename T, typename... Args>
+  T* emplace_impl(map<String, T>& c, String name, Args&&... args) {
+    auto res = c.emplace(piecewise_construct, forward_as_tuple(name),
+                         forward_as_tuple(name, args...));
+    ensure(res.second, "Duplicated definition");
+    return &res.first->second;
   }
 };
 
@@ -60,17 +82,14 @@ class FunctionNode {
   vector<Variable*> args;  // arguments
   BlockNode body;
 
-  FunctionNode(Environment* parent) : body(parent) {}
+  FunctionNode(String name, Environment* parent) : name(name), body(parent) {}
 };
 
 class FileNode {
  public:
   Environment env;
 
-  FileNode() : env(nullptr) {
-    env.types.emplace(piecewise_construct, forward_as_tuple("int"),
-                      forward_as_tuple("int", 4));
-  }
+  FileNode() : env(nullptr) { env.emplace_type("int", 4); }
 
   void codegen() const { cerr << "file codegen\n"; }
 };
@@ -88,24 +107,15 @@ void handle_function(void* generic, const List& list) {
   cerr << "handle_function\n";
   auto env = static_cast<Environment*>(generic);
   auto funcname = list[1].get_symbol();
-  ensure(env->functions.count(funcname) == 0, "Duplicate function definition");
-  FunctionNode* func =
-      &env->functions.emplace(piecewise_construct, forward_as_tuple(funcname),
-                              forward_as_tuple(env)).first->second;
-  func->name = funcname;
+  FunctionNode* func = env->emplace_func(funcname, env);
   env = &func->body.env;
   for (size_t i = 0; i < list[2].size(); i++) {
     ensure(list[2][i].size() == 2, "Invalid argument declaration");
-    auto type = env->lookup_type(list[2][i][0].get_symbol());
-    ensure(type, "No such type");
+    auto type = env->must_lookup_type(list[2][i][0].get_symbol());
     auto name = list[2][i][1].get_symbol();
-    ensure(env->vars.count(name) == 0, "Duplicate variable definition");
-    func->args.push_back(
-        &env->vars.emplace(piecewise_construct, forward_as_tuple(name),
-                           forward_as_tuple(name, type)).first->second);
+    func->args.push_back(env->emplace_var(name, type));
   }
-  auto type = env->lookup_type(list[3].get_symbol());
-  ensure(type, "No such type");
+  auto type = env->must_lookup_type(list[3].get_symbol());
   func->return_type = type;
   for (size_t i = 4; i < list.size(); i++) {
     parse(&func->body, list[i]);
